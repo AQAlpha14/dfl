226e1de050818aba4db4bd79e51ec769ad68804b
AQ Office <126751819+AQAlpha14@users.noreply.github.com>
2026-02-10 19:57:19 +0500

    sdfd

--- view/ListingPage.tsx
+++ view/ListingPage.tsx
@@ -1,6 +1,6 @@
+import FeaturedCards from "@/components/cards/FeaturedCards";
 import SearchBar from "@/components/FormFields/SearchBar";
 import Typography from "@/components/Typography";
-
 
 const ListingPage = () => {
   return (
@@ -12,13 +12,18 @@ const ListingPage = () => {
           <div className="pt-10">
             <div className="pb-8">
               <Typography as="h2" size="lg" weight="semibold">
-              Residential Properties for rent in UAE
-            </Typography>
+                Residential Properties for rent in UAE
+              </Typography>
             </div>
-           <div className="grid lg:grid-cols-3 grid-cols-1 gap-4">
-              <div className="bg-gray-100 h-48 rounded-lg"></div>
-              <div className="bg-gray-100 h-48 rounded-lg"></div>
-           </div>
+            <div className="grid lg:grid-cols-3 grid-cols-1 gap-4">
+              <div className="col-span-2">
+            <div className="grid lg:grid-cols-2 grid-cols-1 gap-4">
+                {Array.from({ length: 16 }).map((_, index) => (
+                  <div key={index} className="bg-gray-300 w-96 h-16"></div>
+                ))}
+              </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
